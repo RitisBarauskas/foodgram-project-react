9a64b2dc29c78f85b9ad7c352f017b47955024c4
ritis <perm300ru@yandex.ru>
2021-10-06 07:26:28 +0500

    Доработана вьюха рецептов

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import TagViewSet, RecipeViewSet, IngredientViewSet
+from .views import TagViewSet, RecipeViewSet, IngredientViewSet, FavoriteViewSet
 
 
 router = DefaultRouter()
@@ -11,5 +11,6 @@ router.register('tags', TagViewSet, basename='tags')
 router.register('recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
+    path('<str:pk>/favorite/', FavoriteViewSet.as_view(), name='favorite'),
     path('', include(router.urls)),
 ]
